7bc172759a4b69af5569e208ceca4ed440b7df94
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-11 19:30:47 +0530

    vercel file added

--- frontend/src/Components/QAPlace.jsx
+++ frontend/src/Components/QAPlace.jsx
@@ -23,7 +23,7 @@ function QAPlace() {
     localStorage.setItem('answers', JSON.stringify([]));
   }
 
-  const socket = io("https://sbtebotbackend.vercel.app", {
+  const socket = io("https://sbtebot-1.onrender.com", {
     withCredentials: true
   });
 
