a4dd9ee56981deee93cc69867af0e95ec00c6191
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-11 18:16:20 +0530

    vercel file added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import QAPlace from './Components/QAPlace'
 function App() {
 
   return (
-
     <>
       <div className='bg-cyan-50 overflow-hidden relative' style={{maxHeight: 'calc(100vh - 3.8rem)', minHeight: 'calc(100vh - 3.8rem)'}}>
         <QAPlace />
