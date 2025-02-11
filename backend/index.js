5a9edeff58c0d5b550beb9a7260766d5098a1cd1
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-11 21:40:35 +0530

    vercel file added

--- backend/index.js
+++ backend/index.js
@@ -11,22 +11,15 @@ dotenv.config();
 const server = http.createServer(app);
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://sbtebot.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://sbtebot.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     }
